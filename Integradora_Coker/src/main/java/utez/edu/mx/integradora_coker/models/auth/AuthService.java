a915856f63c6ad0046bbbd89f7c1677b6e1e9ad0
EmiCort17 <20223tn119@utez.edu.mx>
2025-03-23 17:56:41 -0600

    Login ya funcional

--- Integradora_Coker/src/main/java/utez/edu/mx/integradora_coker/models/auth/AuthService.java
+++ Integradora_Coker/src/main/java/utez/edu/mx/integradora_coker/models/auth/AuthService.java
@@ -2,6 +2,7 @@ package utez.edu.mx.integradora_coker.models.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import utez.edu.mx.integradora_coker.kernel.CustomResponse;
@@ -17,14 +18,17 @@ public class AuthService {
     private UserRepository userRepository;
 
     @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
     private CustomResponse customResponse;
 
     @Transactional(readOnly = true)
     public ResponseEntity<?> login(LoginDTO dto) {
-        Optional<UserBean> found = userRepository.findByUsernameAndPassword(dto.getUsername(), dto.getPassword());
+        Optional<UserBean> found = userRepository.findByUsername(dto.getUsername());
 
-        if (found.isEmpty()) {
-            return customResponse.get400Response(404);
+        if (found.isEmpty() || !passwordEncoder.matches(dto.getPassword(), found.get().getPassword())) {
+            return customResponse.get400Response(404); // Usuario o contraseña incorrectos
         } else {
             UserBean user = found.get();
             return customResponse.getOkResponse("bearertoken." + user.getUsername() + ".voidtoken");
